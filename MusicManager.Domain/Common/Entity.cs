096e38de22f91d2d43020aa65a9b622196b8599f
Vazovsk1y <anolrey.b@gmail.com>
2023-06-19 11:58:49 +0300

    Add project files.

---
+++ MusicManager.Domain/Common/Entity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicManager.Domain.Common;
+
+public abstract class Entity
+{
+    public Guid Id { get; private init; }
+
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+        {
+            return false;
+        }
+
+        if (obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        if (obj is not Entity entity)
+        {
+            return false;
+        }
+
+        return entity.Id == Id;
+    }
+
+    public override int GetHashCode() => Id.GetHashCode();
+}
