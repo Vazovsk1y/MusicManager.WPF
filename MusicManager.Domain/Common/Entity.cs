48231ec53634380dbaad4a023682847e5cff3452
Vazovsk1y <anolrey.b@gmail.com>
2023-06-20 18:05:25 +0300

    -modified Id setting in base entity class.

--- MusicManager.Domain/Common/Entity.cs
+++ MusicManager.Domain/Common/Entity.cs
@@ -1,19 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MusicManager.Domain.Common;
+﻿namespace MusicManager.Domain.Common;
 
 public abstract class Entity
 {
+    #region --Fields--
+
+
+
+    #endregion
+
+    #region --Properties--
+
     public Guid Id { get; private init; }
 
-    protected Entity(Guid id)
+    #endregion
+
+    #region --Constructors--
+
+    protected Entity()
     {
-        Id = id;
+        Id = Guid.NewGuid();
     }
+
+    #endregion
+
+    #region --Methods--
 
     public override bool Equals(object? obj)
     {
@@ -36,4 +46,6 @@ public abstract class Entity
     }
 
     public override int GetHashCode() => Id.GetHashCode();
+
+    #endregion
 }
