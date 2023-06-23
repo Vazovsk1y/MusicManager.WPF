68aa3b18f119dd7d663bab76d255742d3b50ed03
Vazovsk1y <anolrey.b@gmail.com>
2023-06-23 13:50:39 +0300

    -added domain logic to movie class.

--- MusicManager.Domain/Models/Movie.cs
+++ MusicManager.Domain/Models/Movie.cs
@@ -1,32 +1,86 @@
 ﻿using MusicManager.Domain.Common;
+using MusicManager.Domain.Shared;
 using MusicManager.Domain.ValueObjects;
 
 namespace MusicManager.Domain.Models;
 
 public class Movie : Entity
 {
+    #region --Fields--
+
     private readonly List<Disc> _discs = new();
 
-    public Movie(
-        Guid id, 
-        string title, 
-        ProductionInfo productionInfo, 
-        DirectorInfo directorInfo) : base(id)
-    {
-        Title = title;
-        ProductionInfo = productionInfo;
-        DirectorInfo = directorInfo;
-    }
+    #endregion
 
-    public EntityDirectoryInfo? EntityDirectoryInfo { get; private set; }
+    #region --Properties--
 
     public Guid SongwriterId { get; private set; }
 
+    public EntityDirectoryInfo? EntityDirectoryInfo { get; private set; }
+
+    public DirectorInfo? DirectorInfo { get; private set; }
+
     public ProductionInfo ProductionInfo { get; private set; }
 
-    public DirectorInfo DirectorInfo { get; private set; }
-
-    public string Title { get; private set; }
+    public string Title { get; private set; } = string.Empty;
 
     public IReadOnlyCollection<Disc> Discs => _discs.ToList();
+
+    #endregion
+
+    #region --Constructors--
+
+    private Movie() { ProductionInfo = ProductionInfo.None; }
+
+    #endregion
+
+    #region --Methods--
+
+    public static Result<Movie> Create(string title, string productionYear, string productionCountry, string directoryName, string directoryFullPath)
+    {
+        var creationResult = Create(title, productionYear, productionCountry);
+        if (creationResult.IsFailure)
+        {
+            return creationResult;
+        }
+
+        var settingDirInfoResult = creationResult.Value.SetDirectoryInfo(directoryName, directoryFullPath);
+
+        return settingDirInfoResult.IsFailure ?
+            Result.Failure<Movie>(settingDirInfoResult.Error)
+            :
+            creationResult.Value;
+    }
+
+    public static Result<Movie> Create(string title, string productionYear, string productionCountry)
+    {
+        var prodInfoResult = ProductionInfo.Create(productionCountry, productionYear);
+
+        return prodInfoResult.IsFailure ? Result.Failure<Movie>(prodInfoResult.Error) : new Movie
+        {
+            Title = title,
+            ProductionInfo = prodInfoResult.Value
+        };
+    }
+
+    public Result SetParent(Songwriter songwriter)
+    {
+        SongwriterId = songwriter.Id;
+        return Result.Success();
+    }
+
+    public Result SetDirectoryInfo(string name, string fullPath)
+    {
+        var result = EntityDirectoryInfo.Create(name, fullPath);
+
+        if (result.IsFailure)
+        {
+            return Result.Failure(result.Error);
+        }
+
+        EntityDirectoryInfo = result.Value;
+        return Result.Success();
+    }
+
+    #endregion
 }
