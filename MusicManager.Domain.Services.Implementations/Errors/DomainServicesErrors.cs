4c0e760846c79440ee4d32aa56cadc0c902b66df
Vazovsk1y <anolrey.b@gmail.com>
2023-06-26 11:30:18 +0300

    -added implementations errors.

---
+++ MusicManager.Domain.Services.Implementations/Errors/DomainServicesErrors.cs
@@ -0,0 +1,9 @@
+﻿using MusicManager.Domain.Shared;
+
+namespace MusicManager.Domain.Services.Implementations.Errors
+{
+    internal static class DomainServicesErrors
+    {
+        public static Error PassedDirectoryNamedIncorrect(string directoryPath) => new($"Passed directory [{directoryPath}] wasn't named correct.");
+    }
+}
