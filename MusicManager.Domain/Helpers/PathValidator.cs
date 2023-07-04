92cf4dc45c5775933c378cd91d4164b73e24126d
Vazovsk1y <anolrey.b@gmail.com>
2023-07-04 10:19:33 +0300

    -added description to static path validator method.

--- MusicManager.Domain/Helpers/PathValidator.cs
+++ MusicManager.Domain/Helpers/PathValidator.cs
@@ -5,11 +5,16 @@ namespace MusicManager.Domain.Helpers;
 public static partial class PathValidator
 {
     [GeneratedRegex(@"^[a-zA-Z]:\\(?:[^\\/:*?""<>|\r\n]+\\)*[^\\/:*?""<>|\r\n]*$")]
-    private static partial Regex PathNamingRules();
+    private static partial Regex WindowsPathsNamingRules();
 
     [GeneratedRegex(@"^\.{1,}$")]
     private static partial Regex OnlyDotsInPathComponent();
 
+    /// <summary>
+    /// Checks if the passed path is valid. Available only full paths, not relative.
+    /// </summary>
+    /// <param name="path">The full path of file or directory.</param>
+    /// <returns>true - if valid, otherwise false.</returns>
     public static bool IsValid(string path)
     {
         if (string.IsNullOrWhiteSpace(path))
@@ -22,7 +27,7 @@ public static partial class PathValidator
             return false;
         }    
 
-        bool isPathNamingRulesMatches = PathNamingRules().IsMatch(path);
+        bool isPathNamingRulesMatches = WindowsPathsNamingRules().IsMatch(path);
         if (isPathNamingRulesMatches)
         {
             var pathComponents = path.Split(
