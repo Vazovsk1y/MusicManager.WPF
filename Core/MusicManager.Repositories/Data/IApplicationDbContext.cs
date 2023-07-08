ae784ffb52d3845d491eaff39d2798617de8f376
Vazovsk1y <anolrey.b@gmail.com>
2023-07-08 17:14:46 +0300

    -added repositories project.

---
+++ Core/MusicManager.Repositories/Data/IApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicManager.Domain.Entities;
+using MusicManager.Domain.Models;
+
+namespace MusicManager.Repositories.Data;
+
+public interface IApplicationDbContext
+{
+    public DbSet<Songwriter> Songwriters { get; set; }
+
+    public DbSet<Movie> Movies { get; set; }
+
+    public DbSet<MovieRelease> MovieReleases { get; set; }
+
+    public DbSet<Compilation> Compilations { get; set; }
+
+    public DbSet<Song> Songs { get; set; }
+
+    public DbSet<PlaybackInfo> PlaybackInfos { get; set; }
+
+    public DbSet<Cover> Covers { get; set; }
+}
