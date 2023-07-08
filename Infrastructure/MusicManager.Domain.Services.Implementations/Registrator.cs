5e051c5319e37fe3dae41b53a449e3a6d151b4eb
Vazovsk1y <anolrey.b@gmail.com>
2023-07-08 17:32:47 +0300

    -added domainServices registrator.

---
+++ Infrastructure/MusicManager.Domain.Services.Implementations/Registrator.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MusicManager.Domain.Services.Implementations
+{
+    public static class Registrator
+    {
+        public static void AddDomainServices(this IServiceCollection services) => services
+            .AddTransient<ICueFileInteractor, CueFileInteractor>()
+            .AddTransient<IPathToSongwriterService, DirectoryToSongwriterService>()
+            .AddTransient<IPathToMovieService, DirectoryToMovieService>()
+            .AddTransient<IPathToCompilationService, DirectoryToCompilationService>()
+            .AddTransient<IPathToMovieReleaseService, DirectoryToMovieReleaseService>()
+            .AddTransient<IPathToSongService, FileToSongService>()
+            ;
+    }
+}
