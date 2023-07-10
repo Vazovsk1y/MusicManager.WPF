f033c2a6b2090ca7835d1f3f0f0f07350bc42221
Vazovsk1y <anolrey.b@gmail.com>
2023-07-10 18:09:24 +0300

    -modified movieRelease repository.

--- Core/MusicManager.Repositories/IMovieReleaseRepository.cs
+++ Core/MusicManager.Repositories/IMovieReleaseRepository.cs
@@ -1,8 +1,10 @@
-﻿using MusicManager.Domain.Models;
+﻿using MusicManager.Domain.Common;
+using MusicManager.Domain.Models;
 using MusicManager.Repositories.Common;
 
 namespace MusicManager.Repositories;
 
 public interface IMovieReleaseRepository : IDiscRepository<MovieRelease>
 {
+    Task<MovieRelease?> GetReleaseWithMoviesAsync(DiscId id, CancellationToken cancellation = default);
 }
