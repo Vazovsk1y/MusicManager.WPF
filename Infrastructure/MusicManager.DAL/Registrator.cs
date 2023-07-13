87da7804fcd8b867703fc74b2f2148c440d0cc01
Vazovsk1y <anolrey.b@gmail.com>
2023-07-13 19:34:56 +0300

    -added Dal layer registrator.

---
+++ Infrastructure/MusicManager.DAL/Registrator.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MusicManager.DAL.Repositories;
+using MusicManager.Repositories;
+using MusicManager.Repositories.Data;
+
+namespace MusicManager.DAL;
+
+public static class Registrator
+{
+    public static void AddDAL(this IServiceCollection services) => services
+        .AddScoped<ISongwriterRepository, SongwriterRepository>()
+        .AddScoped<IMovieRepository, MovieRepository>()
+        .AddScoped<ICompilationRepository, CompilationRepository>()
+        .AddScoped<IMovieReleaseRepository, MovieReleaseRepository>()
+        .AddScoped<ISongRepository, SongRepository>()
+        .AddScoped<IUnitOfWork, UnitOfWork>()
+        ;
+}
