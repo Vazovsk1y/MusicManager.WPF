d5b97645777dd3bd6262e9ff27e97401417e2ad9
Vazovsk1y <anolrey.b@gmail.com>
2023-07-13 19:35:21 +0300

    -added AppServices layer registrator.

---
+++ Infrastructure/MusicManager.Services.Implementations/Registrator.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MusicManager.Services.Implementations;
+
+public static class Registrator
+{
+    public static void AddAppServices(this IServiceCollection services) => services
+        .AddTransient<ISongwriterService, SongwriterService>()
+        .AddTransient<IMovieService, MovieService>()
+        .AddTransient<ICompilationService, CompilationService>()
+        .AddTransient<IMovieReleaseService, MovieReleaseService>()
+        .AddTransient<ISongService, SongService>()
+        ;
+}
