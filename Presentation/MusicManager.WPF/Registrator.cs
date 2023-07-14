484ad2fbafb61c107a33230d2648bb99dbec7318
Vazovsk1y <anolrey.b@gmail.com>
2023-07-14 17:03:59 +0300

    -added wpf project registrator.

---
+++ Presentation/MusicManager.WPF/Registrator.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+using System;
+using MusicManager.WPF.ViewModels.Base;
+
+namespace MusicManager.WPF;
+
+internal static class Registrator
+{
+    public static void AddWPF(this IServiceCollection services)
+    {
+
+    }
+}
+
+internal static class GenericWindowRegistrator
+{
+    public static IServiceCollection AddWindowTransient<TWindow, TViewModel>(this IServiceCollection services)
+            where TViewModel : ViewModel
+            where TWindow : Window
+            => services.AddTransient(s =>
+            {
+                var viewModel = s.GetRequiredService<TViewModel>();
+                var window = Activator.CreateInstance<TWindow>();
+                window.DataContext = viewModel;
+                return window;
+            });
+
+    public static IServiceCollection AddWindowSingleton<TWindow, TViewModel>(this IServiceCollection services)
+            where TViewModel : ViewModel
+            where TWindow : Window
+            => services.AddSingleton(s =>
+            {
+                var viewModel = s.GetRequiredService<TViewModel>();
+                var window = Activator.CreateInstance<TWindow>();
+                window.DataContext = viewModel;
+                return window;
+            });
+}
+
