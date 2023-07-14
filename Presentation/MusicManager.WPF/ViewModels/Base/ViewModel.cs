2cd8330596a6c77184f1ee9774ed1fae45e0cc89
Vazovsk1y <anolrey.b@gmail.com>
2023-07-14 16:41:45 +0300

    -added base viewModel implementation.

---
+++ Presentation/MusicManager.WPF/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MusicManager.WPF.ViewModels.Base;
+
+internal abstract class ViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? callerName = null) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(callerName));
+
+    protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string? callerName = null)
+    {
+        if (Equals(field, value))
+        {
+            return false; 
+        }
+
+        field = value;
+        OnPropertyChanged(callerName);
+        return true;
+    }
+}
