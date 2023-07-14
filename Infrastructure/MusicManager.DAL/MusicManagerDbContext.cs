1bc80a9dc8dc3a49b5794b047127ac781d692071
Vazovsk1y <anolrey.b@gmail.com>
2023-07-14 19:58:27 +0300

    -added application Db context implementation.

---
+++ Infrastructure/MusicManager.DAL/MusicManagerDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicManager.Domain.Entities;
+using MusicManager.Domain.Models;
+using MusicManager.Repositories.Data;
+using System.Reflection;
+
+namespace MusicManager.DAL;
+
+public class MusicManagerDbContext : DbContext, IApplicationDbContext
+{
+    public DbSet<Songwriter> Songwriters { get; set; }
+
+    public DbSet<Movie> Movies { get; set; }
+
+    public DbSet<MovieRelease> MovieReleases { get; set; }
+
+    public DbSet<Compilation> Compilations { get; set; }
+
+    public DbSet<Song> Songs { get; set; }
+
+    public DbSet<PlaybackInfo> PlaybackInfos { get; set; }
+
+    public DbSet<Cover> Covers { get; set; }
+
+    public MusicManagerDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
--- Infrastructure/MusicManager.DAL/Registrator.cs
+++ Infrastructure/MusicManager.DAL/Registrator.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using MusicManager.DAL.Repositories;
 using MusicManager.Repositories;
 using MusicManager.Repositories.Data;
@@ -7,12 +9,29 @@ namespace MusicManager.DAL;
 
 public static class Registrator
 {
-    public static IServiceCollection AddDAL(this IServiceCollection services) => services
+    public static IServiceCollection AddDAL(this IServiceCollection services, IConfiguration configuration) => services
         .AddScoped<ISongwriterRepository, SongwriterRepository>()
         .AddScoped<IMovieRepository, MovieRepository>()
         .AddScoped<ICompilationRepository, CompilationRepository>()
         .AddScoped<IMovieReleaseRepository, MovieReleaseRepository>()
         .AddScoped<ISongRepository, SongRepository>()
         .AddScoped<IUnitOfWork, UnitOfWork>()
+        .AddDbContext<IApplicationDbContext, MusicManagerDbContext>(options =>
+        {
+            var dbType = configuration["Type"];
+            switch (dbType)
+            {
+                case "MSSQL":
+                    {
+                        options
+                        .UseSqlServer(configuration.GetConnectionString(dbType))
+                        .UseSnakeCaseNamingConvention();
+                        return;
+                    }
+
+                case null: throw new InvalidOperationException("Undefined database type.");
+                default: throw new InvalidOperationException($"Database [{dbType}] is not supported.");
+            }
+        })
         ;
 }
