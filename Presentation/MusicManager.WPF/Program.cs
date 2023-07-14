12c4eb5792a147a176fb03833036d995998d4696
Vazovsk1y <anolrey.b@gmail.com>
2023-07-14 16:49:36 +0300

    -added new app start point.

---
+++ Presentation/MusicManager.WPF/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MusicManager.WPF;
+
+internal class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        App app = new App();
+        app.InitializeComponent();
+        app.Run();
+    }
+}
