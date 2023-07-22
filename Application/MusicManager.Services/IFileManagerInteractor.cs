a152313cb91b107c05f9b980b6d393f58deb8f08
Vazovsk1y <anolrey.b@gmail.com>
2023-07-22 13:00:13 +0300

    -added fileManagerInteractor interace and its base implementation.

---
+++ Application/MusicManager.Services/IFileManagerInteractor.cs
@@ -0,0 +1,10 @@
+﻿using MusicManager.Domain.Shared;
+
+namespace MusicManager.Services;
+
+public interface IFileManagerInteractor
+{
+    Result<DirectoryInfo> GetSelectedDirectory(string description = "Select a directory:");
+
+    Result<FileInfo> GetSelectedFile();
+}
---
+++ Infrastructure/MusicManager.Services.Implementations/FileManagerInteractor.cs
@@ -0,0 +1,29 @@
+﻿using MusicManager.Domain.Shared;
+using System.Windows.Forms;
+
+namespace MusicManager.Services.Implementations;
+
+public class FileManagerInteractor : IFileManagerInteractor
+{
+    public Result<DirectoryInfo> GetSelectedDirectory(string description = "Select a directory:")
+    {
+        using var folderBrowserDialog = new FolderBrowserDialog()
+        {
+            ShowNewFolderButton = false,
+            Description = description,
+        };
+
+        var dialogResult = folderBrowserDialog.ShowDialog();
+        if (dialogResult is DialogResult.OK)
+        {
+            return new DirectoryInfo(folderBrowserDialog.SelectedPath);
+        }
+
+        return Result.Failure<DirectoryInfo>(new Error("No directory was selected."));
+    }
+
+    public Result<FileInfo> GetSelectedFile()
+    {
+        throw new NotImplementedException();
+    }
+}
