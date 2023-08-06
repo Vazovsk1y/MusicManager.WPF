c7652c1e5b870ddc591feee35713b338d22ce8d9
Vazovsk1y <anolrey.b@gmail.com>
2023-08-06 10:42:06 +0300

    -added years and countries helpers.

---
+++ Infrastructure/MusicManager.Utils/ICountriesHelper.cs
@@ -0,0 +1,54 @@
+﻿using System.Globalization;
+
+namespace MusicManager.Utils;
+
+/// <summary>
+/// Register as singleton.
+/// </summary>
+public interface ICountriesHelper
+{
+    IEnumerable<string> Countries { get; }
+}
+
+public class CountryHelper : ICountriesHelper
+{
+    private readonly List<string> _countriesNames = new();
+
+    public IEnumerable<string> Countries
+    {
+        get
+        {
+            foreach (var item in _countriesNames.Order())
+            {
+                yield return item;
+            }
+        }
+    }
+
+    public CountryHelper()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        CultureInfo[] cultures = CultureInfo.GetCultures(CultureTypes.AllCultures & ~CultureTypes.NeutralCultures);
+
+        foreach (var cultureInfo in cultures)
+        {
+            try
+            {
+                RegionInfo region = new(cultureInfo.Name);
+
+                if (!(_countriesNames.Contains(region.EnglishName)))
+                {
+                    _countriesNames.Add(region.EnglishName);
+                }
+            }
+            catch (ArgumentException)
+            {
+                continue;
+            }
+        }
+    }
+}
---
+++ Infrastructure/MusicManager.Utils/IYearsHelper.cs
@@ -0,0 +1,38 @@
+﻿namespace MusicManager.Utils;
+
+/// <summary>
+/// Register as singleton.
+/// </summary>
+public interface IYearsHelper
+{
+    IEnumerable<string> Years { get; }
+}
+
+public class YearsHelper : IYearsHelper
+{
+    private readonly List<string> _years = new();
+
+    public IEnumerable<string> Years
+    {
+        get
+        {
+            foreach (var item in _years)
+            {
+                yield return item;
+            }
+        }
+    }
+
+    public YearsHelper()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        var numbers = Enumerable.Range(1, DateTime.Now.Year).OrderDescending().Select(e => e.ToString());
+        _years.AddRange(numbers);
+    }
+}
+
+
