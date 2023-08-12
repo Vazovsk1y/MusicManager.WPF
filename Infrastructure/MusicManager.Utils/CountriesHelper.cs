ca49e726724e8828079ef187b4920aa076b70bbb
Vazovsk1y <anolrey.b@gmail.com>
2023-08-12 15:09:39 +0300

    -marked counties and years helpers as static classes.

---
+++ Infrastructure/MusicManager.Utils/CountriesHelper.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+
+namespace MusicManager.Utils;
+
+public static class CountryHelper 
+{
+    private static readonly List<string> _countriesNames = new();
+
+    public static IEnumerable<string> Countries
+    {
+        get
+        {
+            foreach (var item in _countriesNames.Order())
+            {
+                yield return item;
+            }
+        }
+    }
+
+    static CountryHelper()
+    {
+        Initialize();
+    }
+
+    private static void Initialize()
+    {
+        CultureInfo[] cultures = CultureInfo.GetCultures(CultureTypes.AllCultures & ~CultureTypes.NeutralCultures);
+
+        foreach (var cultureInfo in cultures)
+        {
+            try
+            {
+                RegionInfo region = new(cultureInfo.Name);
+
+                if (!(_countriesNames.Contains(region.EnglishName)))
+                {
+                    _countriesNames.Add(region.EnglishName);
+                }
+            }
+            catch (ArgumentException)
+            {
+                continue;
+            }
+        }
+    }
+}
