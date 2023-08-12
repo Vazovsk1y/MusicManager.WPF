28ea28de1f7995d8667bff134b0945d3407f398f
Vazovsk1y <anolrey.b@gmail.com>
2023-08-12 19:42:18 +0300

    -added directory helper.

---
+++ Infrastructure/MusicManager.Utils/DirectoryHelper.cs
@@ -0,0 +1,35 @@
+﻿namespace MusicManager.Utils;
+
+public static class DirectoryHelper
+{
+    public static string LocalApplicationDataPath => Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+    public static DirectoryInfo CreateIfNotExists(string path)
+    {
+        ArgumentNullException.ThrowIfNull(nameof(path));
+
+        var directoryInfo = new DirectoryInfo(path);
+        if (!directoryInfo.Exists) 
+        {
+            directoryInfo.Create();
+        }
+
+        return directoryInfo;
+    }
+
+    public static (bool isCreated, string message) TryToCreateIfNotExists(string path, out DirectoryInfo? directoryInfo)
+    {
+        try
+        {
+            directoryInfo = CreateIfNotExists(path);
+            return (true, string.Empty);
+        }
+        catch(Exception ex)
+        {
+            directoryInfo = null;
+            return (false, ex.Message);
+        }
+    }
+}
+
+
